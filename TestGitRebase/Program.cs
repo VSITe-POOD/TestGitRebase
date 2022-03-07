4bbf8be0184f4ac373fb3846fe2cd1841e2451ba
Leon Pleslic <LPleslic@dspace.hr>
2022-03-07 20:37:28 +0100

    add method1

--- TestGitRebase/Program.cs
+++ TestGitRebase/Program.cs
@@ -10,6 +10,12 @@ namespace TestGitRebase
     {
         static void Main(string[] args)
         {
+            Method1();
+        }
+
+        static void Method1()
+        {
+            Console.WriteLine("Method2");
         }
     }
 }
