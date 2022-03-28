8a8e3c8f9d937fbabfdd0e72f5ae4f2d1a23666f
Antonio Brkic <ABrkic@dspace.hr>
2022-03-28 19:23:13 +0200

    Add location

--- TestGitRebase/Program.cs
+++ TestGitRebase/Program.cs
@@ -72,6 +72,7 @@ namespace TestGitRebase
         static void ABrkic()
         {
             Console.WriteLine("My name is Antonio Brkić");
+            Console.WriteLine("I live in Zagreb");
         }
 
         static void AVrljic()
